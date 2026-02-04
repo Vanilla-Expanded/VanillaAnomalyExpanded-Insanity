232140cfb0132740e1794b9248a8ff2fd7db3bf1
juanosarg <31105173+juanosarg@users.noreply.github.com>
2026-02-04 11:54:51 +0100

    HediffComp_ReduceSanity

---
+++ 1.6/Source/HediffComps/HediffComp_ReduceSanity.cs
@@ -0,0 +1,23 @@
+﻿using Verse;
+
+namespace VAEInsanity
+{
+    public class HediffComp_ReduceSanity : HediffComp
+    {
+        public const int day = 60000;
+       
+       
+        public override void CompPostTick(ref float severityAdjustment)
+        {
+            base.CompPostTick(ref severityAdjustment);
+
+            if (this.parent.pawn.IsHashIntervalTick(day))
+            {
+                this.parent.pawn.SanityGain(-0.01f, "VAEI_VoidWeapon".Translate());
+                
+            }
+        }
+
+       
+    }
+}
